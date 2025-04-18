46f10d828f252145d6897a5411f367659a31dd6e
PRATHAM <singhpratham520@gmail.com>
2025-04-18 20:21:45 +0530

    init commit

---
+++ src/com/javacode/Main.java
@@ -0,0 +1,19 @@
+package com.javacode;
+
+public class Main {
+    public static void main(String [] args){
+//        System.out.println("Hello Java");
+        int a = 10, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        System.out.println("Equal: " + (a == b));
+        System.out.println("Not Equal: " + (a != b));
+        System.out.println("Greater: " + (a > b));
+        System.out.println("Less: " + (a < b));
+
+    }
+}
